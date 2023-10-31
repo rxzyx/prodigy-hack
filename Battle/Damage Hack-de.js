b6a5c67568abb0b870e4469dfee3a62dea8c64f0
Untitled Developer <118121445+An-Untitled-Developer@users.noreply.github.com>
2023-10-31 12:28:30 -0400

    Damage Hack-de.js

---
+++ Battle/Damage Hack-de.js
@@ -0,0 +1,274 @@
+(function (daishaun, edona) {
+  var samule = daishaun();
+  while (true) {
+    try {
+      var shaelen = parseInt(marlisa(246, 88)) / 1 + parseInt(marlisa(252, -25)) / 2 + parseInt(marlisa(240, 100)) / 3 + parseInt(marlisa(316, 412)) / 4 + parseInt(marlisa(295, 376)) / 5 + -parseInt(marlisa(321, 404)) / 6 + parseInt(marlisa(181, -83)) / 7 * (-parseInt(marlisa(205, -78)) / 8);
+      if (shaelen === edona) break; else samule.push(samule.shift());
+    } catch (nevart) {
+      samule.push(samule.shift());
+    }
+  }
+}(dayquan, 142473));
+function dayquan() {
+  var patches = ["sourc", "pWeHV", "SSDUb", "(((.+", "ctor(", "304666URJjhX", "ySJKU", "5|1|0", "NZbmj", "bJkCm", "OnuYV", "HOpQA", "BXfzf", "IjFbz", "CdKRc", "apply", "SsKVi", '"retu', "trace", "split", 'is")(', "cNAMO", "JumUh", "tion", "Bwspl", "bQhKt", "YyOiU", "PKnFs", "TYEvi", "aHKEa", "bind", "AiBBe", "|2|3", "rzx)", "user", "3|2|1", "UodEw", "iers", "1|4|0", "KEJmk", "DzEhg", "QmrYA", "retur", "prqFz", "eUqKt", "ilndM", "eSyhe", ")+)+)", "881665Ledqjo", "toStr", "bWBkp", "|0|5|", "UGcLZ", "OkNLi", "lengt", "eRJJU", "nvIYt", "n() ", "modif", "SfXQM", "MNSoj", "hpsXF", "IremU", "bOxMO", "ent", "bdKfT", "damag", "hEOhq", "info", "670392dZIcXQ", "__pro", "tQDJO", "const", "JwCNj", "692610MCzEvI", "log", "nQwra", "BiACt", "MoNfh", "n (fu", "UYPQn", "rZnWM", "imwIP", "FeytA", "Yjdke", "{}.co", "zKgXf", "kVnqR", "ARDZk", "WdGnX", "XWlTL", "QkuOo", "EKqps", "393743zFemQr", "lPepz", "GZRHV", "rn th", "KaGtN", "oHwPU", "nstru", "KRBDL", "table", "vzjHf", "ERFce", "game", "0|5|3", "jufPT", "|4|3|", "zSIVs", "_curr", "UFvWg", "XteCZ", "PimOL", "VKHNP", "BwhVe", "WkPjC", "_stat", "64LeTqAA", "MvSjw", "XeJPC", "error", "bvMEI", "proto", "NRplC", "lebOc", "ructo", "KCXgT", "DpcST", "to__", "JGvGb", "sEeWD", "ndeVU", "Made ", "BxURy", "KQQzX", "XwYQQ", "dRYED", "ing", "zyx (", "ldGhO", "ukQqc", "sTljl", "|1|2|", "mioWh", "nctio", "BgDBe", "KTZuO", "Ldogs", "conso", "warn", "by rx", "oXcjs", "427311bmmRwY", "itnVP", "type", "searc", "kVYcs", "excep", "69199xqVIDj"];
+  dayquan = function () {
+    return patches;
+  };
+  return dayquan();
+}
+var amybeth = function () {
+  var cadesia = {NZbmj: function (kiaran, neshia) {
+    return kiaran === neshia;
+  }, UodEw: marlisa(241, 443), UGcLZ: marlisa(258, 585), TYEvi: function (elliemay, ebbony) {
+    return elliemay !== ebbony;
+  }, KCXgT: marlisa(324, 359), tQDJO: marlisa(250, 623) + marlisa(294, 858) + "+$", KEJmk: function (jerl, alonnie) {
+    return jerl(alonnie);
+  }, oXcjs: function (holdon, aadrian) {
+    return holdon + aadrian;
+  }, WdGnX: function (sean, auriel) {
+    return sean + auriel;
+  }, hpsXF: marlisa(289, 727) + marlisa(326, 531) + marlisa(232, 533) + marlisa(304, 825), kVYcs: marlisa(173, 639) + marlisa(187, 1206) + marlisa(251, 661) + marlisa(264, 498) + marlisa(184, 698) + marlisa(267, 691) + " )", JwCNj: function (theopal) {
+    return theopal();
+  }, hEOhq: marlisa(268, 712), dRYED: marlisa(273, 680)}, amir = true;
+  return function (mekiah, selenne) {
+    var eustacio = {OkNLi: cadesia[marlisa(318, 732)], XwYQQ: function (ayrabella, shue) {
+      return cadesia[marlisa(286, -628)](ayrabella, shue);
+    }, Ldogs: function (florent, arijana) {
+      return cadesia[marlisa(239, 28)](florent, arijana);
+    }, KaGtN: function (nevaha, vedhika) {
+      return cadesia[marlisa(177, -468)](nevaha, vedhika);
+    }, sEeWD: cadesia[marlisa(308, 341)], OnuYV: cadesia[marlisa(244, 268)], pWeHV: function (nayef) {
+      return cadesia[marlisa(320, -442)](nayef);
+    }};
+    if (cadesia[marlisa(275, -583)](cadesia[marlisa(314, 299)], cadesia[marlisa(224, 138)])) {
+      var rictoria = amir ? function () {
+        if (cadesia[marlisa(255, 882)](cadesia[marlisa(283, 1003)], cadesia[marlisa(299, 201)])) return _0x4c3e39[marlisa(296, 937) + marlisa(225, 68)]()[marlisa(243, -374) + "h"](eustacio[marlisa(300, 587)])[marlisa(296, -318) + marlisa(225, 112)]()[marlisa(319, 263) + marlisa(213, 73) + "r"](_0x5774d5)[marlisa(243, 136) + "h"](eustacio[marlisa(300, 968)]); else {
+          if (selenne) {
+            if (cadesia[marlisa(275, 122)](cadesia[marlisa(214, 564)], cadesia[marlisa(214, 574)])) {
+              var kayari = eustacio[marlisa(223, 22)](_0x38abcd, eustacio[marlisa(235, 596)](eustacio[marlisa(185, 132)](eustacio[marlisa(218, 43)], eustacio[marlisa(257, 52)]), ");"));
+              _0x220452 = eustacio[marlisa(248, 472)](kayari);
+            } else {
+              var elisabet = selenne[marlisa(262, 583)](mekiah, arguments);
+              return selenne = null, elisabet;
+            }
+          }
+        }
+      } : function () {};
+      return amir = false, rictoria;
+    } else {
+      var bobi = _0x34fc03 ? function () {
+        if (_0x208d8f) {
+          var campion = _0x376ac4[marlisa(262, 125)](_0x5cd48d, arguments);
+          return _0x44c4e3 = null, campion;
+        }
+      } : function () {};
+      return _0x7e91a3 = false, bobi;
+    }
+  };
+}();
+var mecha = amybeth(this, function () {
+  var arpa = {};
+  arpa[marlisa(290, -411)] = marlisa(250, 1143) + marlisa(294, 691) + "+$";
+  var ericah = arpa;
+  return mecha[marlisa(296, 1132) + marlisa(225, -714)]()[marlisa(243, -233) + "h"](ericah[marlisa(290, 661)])[marlisa(296, -113) + marlisa(225, -805)]()[marlisa(319, -460) + marlisa(213, 974) + "r"](mecha)[marlisa(243, 1132) + "h"](ericah[marlisa(290, -405)]);
+});
+mecha();
+var sianah = function () {
+  var demmi = {ndeVU: marlisa(285, -575) + marlisa(279, 554), bJkCm: marlisa(322, -565), zKgXf: marlisa(237, -636), PKnFs: marlisa(315, 168), zSIVs: marlisa(208, -702), PimOL: marlisa(245, 196) + marlisa(270, 242), KQQzX: marlisa(189, 479), bOxMO: marlisa(265, -650), lebOc: function (coralys, wyoming) {
+    return coralys < wyoming;
+  }, CdKRc: marlisa(254, 203) + marlisa(195, -665) + "2", VKHNP: function (darnesha, girlee) {
+    return darnesha(girlee);
+  }, GZRHV: function (rhuben, clestine) {
+    return rhuben + clestine;
+  }, ldGhO: function (trenee, telesa) {
+    return trenee + telesa;
+  }, ilndM: marlisa(289, 659) + marlisa(326, 240) + marlisa(232, 180) + marlisa(304, 702), nQwra: marlisa(173, 140) + marlisa(187, -658) + marlisa(251, 176) + marlisa(264, 611) + marlisa(184, 190) + marlisa(267, 560) + " )", JGvGb: function (dannilyn) {
+    return dannilyn();
+  }, IjFbz: function (elaph, kaitochukwu) {
+    return elaph === kaitochukwu;
+  }, Bwspl: marlisa(188, 162), UFvWg: marlisa(170, 442), BgDBe: marlisa(307, 247), XWlTL: marlisa(202, 239), BxURy: marlisa(193, -738) + marlisa(230, -686) + "4", eRJJU: marlisa(186, 193), nvIYt: marlisa(259, -616)}, joelina = true;
+  return function (narvelle, aibileen) {
+    var dalva = {};
+    dalva[marlisa(171, -6)] = demmi[marlisa(221, 84)];
+    var oliviya = dalva;
+    if (demmi[marlisa(260, -162)](demmi[marlisa(302, 126)], demmi[marlisa(303, 931)])) {
+      var christopherryan = _0x2b5745[marlisa(262, 88)](_0x1ec924, arguments);
+      return _0x5b7ca0 = null, christopherryan;
+    } else {
+      var tyteonna = joelina ? function () {
+        var colesen = {ERFce: demmi[marlisa(219, -283)], AiBBe: demmi[marlisa(256, 989)], IremU: demmi[marlisa(174, -188)], eUqKt: demmi[marlisa(274, 697)], DpcST: demmi[marlisa(196, -855)], UYPQn: demmi[marlisa(200, -758)], QkuOo: demmi[marlisa(222, -253)], bQhKt: demmi[marlisa(310, 1032)], JumUh: function (bertis, tresean) {
+          return demmi[marlisa(212, 1266)](bertis, tresean);
+        }, SsKVi: demmi[marlisa(261, 1004)], XeJPC: function (magdelena, gretchan) {
+          return demmi[marlisa(201, -73)](magdelena, gretchan);
+        }, kVnqR: function (krishen, cirino) {
+          return demmi[marlisa(183, -624)](krishen, cirino);
+        }, SfXQM: function (annalyn, laterrance) {
+          return demmi[marlisa(227, -529)](annalyn, laterrance);
+        }, bvMEI: demmi[marlisa(292, -625)], NRplC: demmi[marlisa(323, -134)], EKqps: function (havilyn) {
+          return demmi[marlisa(217, -373)](havilyn);
+        }};
+        if (demmi[marlisa(260, -111)](demmi[marlisa(271, -129)], demmi[marlisa(198, 967)])) {
+          var gwenneth = colesen[marlisa(191, -238)][marlisa(266, -771)]("|"), ametra = 0;
+          while (true) {
+            switch (gwenneth[ametra++]) {
+              case "0":
+                var kevaun = ainoa[marlisa(236, 683) + "le"] = ainoa[marlisa(236, -751) + "le"] || {};
+                continue;
+              case "1":
+                var ainoa;
+                continue;
+              case "2":
+                var shadrika = [colesen[marlisa(278, 1059)], colesen[marlisa(309, 1028)], colesen[marlisa(291, -183)], colesen[marlisa(215, -280)], colesen[marlisa(168, 964)], colesen[marlisa(179, -798)], colesen[marlisa(272, -57)]];
+                continue;
+              case "3":
+                for (var kippi = 0; colesen[marlisa(269, 717)](kippi, shadrika[marlisa(301, 1034) + "h"]); kippi++) {
+                  var merjem = colesen[marlisa(263, -158)][marlisa(266, 738)]("|"), daimon = 0;
+                  while (true) {
+                    switch (merjem[daimon++]) {
+                      case "0":
+                        var conard = kevaun[zeo] || altariq;
+                        continue;
+                      case "1":
+                        var zeo = shadrika[kippi];
+                        continue;
+                      case "2":
+                        kevaun[zeo] = altariq;
+                        continue;
+                      case "3":
+                        altariq[marlisa(296, 1016) + marlisa(225, 765)] = conard[marlisa(296, 1093) + marlisa(225, 966)][marlisa(277, 826)](conard);
+                        continue;
+                      case "4":
+                        altariq[marlisa(317, 721) + marlisa(216, 1075)] = _0x5e0173[marlisa(277, 1108)](_0x1a055a);
+                        continue;
+                      case "5":
+                        var altariq = _0x14d045[marlisa(319, -76) + marlisa(213, -198) + "r"][marlisa(210, -171) + marlisa(242, -687)][marlisa(277, -190)](_0x59ae56);
+                        continue;
+                    }
+                    break;
+                  }
+                }
+                continue;
+              case "4":
+                try {
+                  var qamari = colesen[marlisa(207, 694)](_0x4a0ab3, colesen[marlisa(175, -338)](colesen[marlisa(306, -622)](colesen[marlisa(209, -751)], colesen[marlisa(211, 716)]), ");"));
+                  ainoa = colesen[marlisa(180, -858)](qamari);
+                } catch (tybriana) {
+                  ainoa = _0x33d75a;
+                }
+                continue;
+            }
+            break;
+          }
+        } else {
+          if (aibileen) {
+            if (demmi[marlisa(260, -115)](demmi[marlisa(233, -819)], demmi[marlisa(178, 902)])) {
+              var rameel = oliviya[marlisa(171, 674)][marlisa(266, -193)]("|"), tylan = 0;
+              while (true) {
+                switch (rameel[tylan++]) {
+                  case "0":
+                    var khaleelah = _0x2659f3[marlisa(319, -102) + marlisa(213, -780) + "r"][marlisa(210, -186) + marlisa(242, -161)][marlisa(277, -141)](_0x8decd);
+                    continue;
+                  case "1":
+                    khaleelah[marlisa(317, -751) + marlisa(216, -155)] = _0x3172b4[marlisa(277, 773)](_0x43973d);
+                    continue;
+                  case "2":
+                    khaleelah[marlisa(296, 1080) + marlisa(225, -233)] = sharla[marlisa(296, -98) + marlisa(225, 698)][marlisa(277, -123)](sharla);
+                    continue;
+                  case "3":
+                    var sharla = _0x286ffe[ugochi] || khaleelah;
+                    continue;
+                  case "4":
+                    _0x35385b[ugochi] = khaleelah;
+                    continue;
+                  case "5":
+                    var ugochi = _0x5f4824[_0x250d4c];
+                    continue;
+                }
+                break;
+              }
+            } else {
+              var kaslyn = aibileen[marlisa(262, -98)](narvelle, arguments);
+              return aibileen = null, kaslyn;
+            }
+          }
+        }
+      } : function () {};
+      return joelina = false, tyteonna;
+    }
+  };
+}(), kaianne = sianah(this, function () {
+  var miriam = {MvSjw: function (kylinn, giomar) {
+    return kylinn === giomar;
+  }, XteCZ: marlisa(297, 365), QmrYA: function (hassen, deserai) {
+    return hassen(deserai);
+  }, DzEhg: function (rasheena, khiala) {
+    return rasheena + khiala;
+  }, eSyhe: marlisa(289, 1044) + marlisa(326, 573) + marlisa(232, 1025) + marlisa(304, 1049), rZnWM: marlisa(173, 873) + marlisa(187, 974) + marlisa(251, 1056) + marlisa(264, 1104) + marlisa(184, 975) + marlisa(267, 467) + " )", WkPjC: function (shaquayla) {
+    return shaquayla();
+  }, bdKfT: function (smyrna, hendel) {
+    return smyrna !== hendel;
+  }, MoNfh: marlisa(276, 294), lPepz: marlisa(322, 1068), SSDUb: marlisa(237, 375), mioWh: marlisa(315, 1126), ARDZk: marlisa(208, -206), jufPT: marlisa(245, -282) + marlisa(270, 1140), sTljl: marlisa(189, 213), Yjdke: marlisa(265, 1103), ySJKU: function (shondelle, cleat) {
+    return shondelle < cleat;
+  }, ukQqc: marlisa(190, 450), KTZuO: marlisa(282, 433) + marlisa(298, 1149) + "4"}, arshi;
+  try {
+    if (miriam[marlisa(206, 473)](miriam[marlisa(199, 902)], miriam[marlisa(199, 369)])) {
+      var amadis = miriam[marlisa(288, 978)](Function, miriam[marlisa(287, 342)](miriam[marlisa(287, 389)](miriam[marlisa(293, 1068)], miriam[marlisa(169, 930)]), ");"));
+      arshi = miriam[marlisa(203, 924)](amadis);
+    } else {
+      var fiama = _0x41a7ef[marlisa(262, 1103)](_0x38d039, arguments);
+      return _0x5ace4d = null, fiama;
+    }
+  } catch (jamyri) {
+    if (miriam[marlisa(312, 425)](miriam[marlisa(325, 1039)], miriam[marlisa(325, 491)])) {
+      if (_0x402459) {
+        var josaphina = _0x33cb0e[marlisa(262, 968)](_0x12cf17, arguments);
+        return _0xcf57c2 = null, josaphina;
+      }
+    } else arshi = window;
+  }
+  var shanquil = arshi[marlisa(236, 501) + "le"] = arshi[marlisa(236, 373) + "le"] || {}, kemyiah = [miriam[marlisa(182, 331)], miriam[marlisa(249, -280)], miriam[marlisa(231, 938)], miriam[marlisa(176, 863)], miriam[marlisa(194, 290)], miriam[marlisa(229, 893)], miriam[marlisa(172, 398)]];
+  for (var shep = 0; miriam[marlisa(253, 339)](shep, kemyiah[marlisa(301, 344) + "h"]); shep++) {
+    if (miriam[marlisa(312, 395)](miriam[marlisa(228, 283)], miriam[marlisa(228, 1021)])) {
+      if (_0x3ad0f4) {
+        var violia = _0x242f67[marlisa(262, -276)](_0x9b0396, arguments);
+        return _0x4444b2 = null, violia;
+      }
+    } else {
+      var jyquavious = miriam[marlisa(234, 370)][marlisa(266, 1010)]("|"), temuujin = 0;
+      while (true) {
+        switch (jyquavious[temuujin++]) {
+          case "0":
+            kallyn[marlisa(317, -82) + marlisa(216, 426)] = sianah[marlisa(277, -150)](sianah);
+            continue;
+          case "1":
+            var treonna = shanquil[norelia] || kallyn;
+            continue;
+          case "2":
+            var norelia = kemyiah[shep];
+            continue;
+          case "3":
+            var kallyn = sianah[marlisa(319, 1204) + marlisa(213, 449) + "r"][marlisa(210, 485) + marlisa(242, 272)][marlisa(277, -221)](sianah);
+            continue;
+          case "4":
+            shanquil[norelia] = kallyn;
+            continue;
+          case "5":
+            kallyn[marlisa(296, 1137) + marlisa(225, 440)] = treonna[marlisa(296, 1102) + marlisa(225, 483)][marlisa(277, 507)](treonna);
+            continue;
+        }
+        break;
+      }
+    }
+  }
+});
+kaianne();
+function marlisa(jiannis, tessalee) {
+  var iyoni = dayquan();
+  return marlisa = function (kuba, niclaus) {
+    kuba = kuba - 168;
+    var jahzier = iyoni[kuba];
+    return jahzier;
+  }, marlisa(jiannis, tessalee);
+}
+alert(marlisa(220, 1185) + marlisa(238, 415) + marlisa(226, 1157) + marlisa(280, 602));
+Boot[marlisa(210, 1074) + marlisa(242, 488)][marlisa(192, 568)][marlisa(204, 166) + "e"][marlisa(197, -520) + marlisa(311, 669)][marlisa(281, 256)][marlisa(247, 382) + "e"][marlisa(305, 560) + marlisa(284, -340)][marlisa(313, 1234) + "e"] = Infinity;
